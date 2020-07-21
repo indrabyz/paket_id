fb4233d7e4f8885d5d758514ac9cec9b01681f5a
andikha <andikha@mitramis.id>
2020-07-21 20:28:25 +0700

    update4

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-#API for Paket.id
-Check the recent version at https://github.com/chriskacerguis/codeigniter-restserver
+# API for Paket.id
+
+Check the recent version at https://github.com/chriskacerguis/codeigniter-restserver53
+
+# get data
+localhost/paketid/index.php/api/destination_data
+
+# get data ($id)
+localhost/paketid/index.php/api/destination_data?id=1
+
+# post data
+localhost/paketid/index.php/api/destination_data
+
+# put data
+localhost/paketid/index.php/api/destination_data (PUT in Postman)
+
+# del data
+localhost/paketid/index.php/api/destination_data (DEL in Postman)
--- application/controllers/api/destination_data.php
+++ application/controllers/api/destination_data.php
@@ -49,16 +49,16 @@ class destination_data extends REST_Controller {
     function index_put() {
         $id = $this->put('id');
         $data = array(
-            'id'    => $this->post('id'),
-            'customer_name'  => $this->post('customer_name'),
-            'customer_address' => $this->post('customer_address'),
-            'customer_email'  => $this->post('customer_email'),
-            'customer_phone' => $this->post('customer_phone'),
-            'customer_address_detail'  => $this->post('customer_address_detail'),
-            'customer_zip_code' => $this->post('customer_zip_code'),
-            'zone_code'  => $this->post('zone_code'),
-            'organization_id' => $this->post('organization_id'),
-            'location_id'  => $this->post('location_id'));
+            'id'    => $this->put('id'),
+            'customer_name'  => $this->put('customer_name'),
+            'customer_address' => $this->put('customer_address'),
+            'customer_email'  => $this->put('customer_email'),
+            'customer_phone' => $this->put('customer_phone'),
+            'customer_address_detail'  => $this->put('customer_address_detail'),
+            'customer_zip_code' => $this->put('customer_zip_code'),
+            'zone_code'  => $this->put('zone_code'),
+            'organization_id' => $this->put('organization_id'),
+            'location_id'  => $this->put('location_id'));
         $this->db->where('id', $id);
         $update = $this->db->update('ujian', $data);
         if ($update) {
